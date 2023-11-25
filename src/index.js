d9f33119f55542c2a051dfc28fd21ad2c08fc62e
Amul Dahal <amuldahal89@gmail.com>
2023-11-25 17:32:55 +0545

    default export for plugin

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import DragulaContainer from "./components/DragulaContainer.vue";
 import DragulaList from "./components/DragulaList.vue";
 import DragulaListItem from "./components/DragulaListItem.vue";
 
-const DragulaVue = {
+export default {
     install(app) {
         app.component("dragula-container", DragulaContainer);
         app.component("dragula-list", DragulaList);
         app.component("dragula-list-item", DragulaListItem);
     },
 };
-
-export { DragulaVue };
